9505df325235661be1f58c461931c887249346ce
shibo <bo.shi@baidao.com>
2018-05-21 01:00:46 +0800

    strStr

---
+++ strStr.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+  return haystack.indexOf(needle);
+};
+//https://leetcode-cn.com/problems/implement-strstr/description/
+
+//emmmm.....明明有这个函数
