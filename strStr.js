fa9cfac32c189065f65eb1c4bdd4d4c74ba9450f
shibo <bo.shi@baidao.com>
2018-05-21 09:03:52 +0800

    更换实现

--- strStr.js
+++ strStr.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-  return haystack.indexOf(needle);
+  if (haystack == null || needle == null || needle.length > haystack.length) {
+    return -1;
+  }
+  if (haystack.length === 0 || needle.length === 0) {
+    return 0;
+  }
+
+  for (let i = 0; i <= (haystack.length - needle.length); i += 1) {
+    if (haystack.substring(i, i + needle.length) === needle) {
+      return i;
+    }
+  }
+
+  return -1;
 };
 //https://leetcode-cn.com/problems/implement-strstr/description/
 
